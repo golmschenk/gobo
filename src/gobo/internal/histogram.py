e50d408bfcacc67516f935a69a8488a172445e5d
golmschenk <greg@olmschenk.com>
2025-11-14 21:55:19 -0500

    Add bin parameter

--- src/gobo/internal/histogram.py
+++ src/gobo/internal/histogram.py
@@ -5,12 +5,12 @@ from bokeh.plotting import figure
 from numpy import typing as npt
 
 
-def create_histogram_figure(array: npt.NDArray) -> figure:
+def create_histogram_figure(array: npt.NDArray, *, bins=30) -> figure:
     figure_ = figure()
-    add_1d_histogram_to_figure(figure_, array)
+    add_1d_histogram_to_figure(figure_, array, bins=bins)
     return figure_
 
 
-def add_1d_histogram_to_figure(figure_, array):
-    hist, edges = np.histogram(array, density=True, bins=30)
+def add_1d_histogram_to_figure(figure_, array, *, bins):
+    hist, edges = np.histogram(array, density=True, bins=bins)
     figure_.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], line_color="white")
