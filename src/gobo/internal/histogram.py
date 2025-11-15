991028635203a57b58cd0355959b0d9b9657171c
golmschenk <greg@olmschenk.com>
2025-11-15 12:48:01 -0500

    Allow histogram generation take in a figure function

--- src/gobo/internal/histogram.py
+++ src/gobo/internal/histogram.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
+from typing import Callable, Any
+
 import numpy as np
 from bokeh.plotting import figure
 from numpy import typing as npt
 
 
-def create_histogram_figure(array: npt.NDArray, *, bins=30) -> figure:
-    figure_ = figure()
+def create_histogram_figure(array: npt.NDArray, *, bins: int = 30, figure_function: Callable[[..., Any], figure] | None = None) -> figure:
+    if figure_function is None:
+        figure_function = figure
+    figure_ = figure_function()
     add_1d_histogram_to_figure(figure_, array, bins=bins)
     return figure_
 
